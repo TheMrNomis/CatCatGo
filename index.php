748262d049d2699d9d53cb99ba14b4e568835163
TheMrNomis <n0m1s@homnomnom.fr>
2015-11-15 10:55:35 +0100

    index.php first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="">
+<head>
+    <meta charset="utf-8">
+    <title>CatCatGo</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
+
+<body>
+    <div id="search-block">
+        <form method="get" action="./search.php">
+            <input type="search" name="q" id="q" />
+            <input type="submit"/>
+        </form>
+    </div>
+</body>
+</html>
