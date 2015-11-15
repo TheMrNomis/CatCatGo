da3260196e413d9203297c19c1e9bf09fd102cde
TheMrNomis <n0m1s@homnomnom.fr>
2015-11-15 13:58:39 +0100

    added style to the homepage

--- index.php
+++ index.php
@@ -8,9 +8,13 @@
 
 <body>
     <div id="search-block">
+        <div id="logo">
+            <img src="./CatCatGo.svg" />
+            <h1>CatCatGo</h1>
+        </div>
         <form method="get" action="./search.php">
-            <input type="search" name="q" id="q" />
-            <input type="submit"/>
+            <input type="search" name="q" id="q" class="textarea" />
+            <input type="submit" class="submit" value="" />
         </form>
     </div>
 </body>
